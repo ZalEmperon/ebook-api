5b368c28a883fa43b70a3fa7d17cf39160a4733e
ZalEmperon <flowgreen173@gmail.com>
2021-08-14 21:57:42 +0700

    Migration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,3 +17,4 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::get('/me', [AuthController::class, 'me']);
+Route::resource('books', BookController::class);
